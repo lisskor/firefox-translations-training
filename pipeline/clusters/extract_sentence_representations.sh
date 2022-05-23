e7bc7d4fde5256b34d8131a44a7fc499f213cea0
lisskor <elizaveta.korotkova@gmail.com>
2022-05-23 19:06:16 +0300

    Add script for extracting sentence embeddings

---
+++ pipeline/clusters/extract_sentence_representations.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+##
+# Extract sentence representations for clustering
+#
+
+set -x
+set -euo pipefail
+
+input_data=$1
+hf_teacher_dir=$2
+batch_size=$3
+layer_num=$4
+output_filename=$5
+
+tmp="${hf_teacher_dir}/tmp"
+mkdir -p "${tmp}"
+
+echo "### Extracting sentence representations for ${input_data}"
+
+# temporarily uncompress input data
+pigz -dc "${input_data}" > "${tmp}/tmp_corpus.txt"
+
+# extract representations from given layer
+python 3rd_party/domain_clusters/extract_sentence_representations.py \
+      --hf-model-dir "${hf_teacher_dir}" --txt-dataset-path "${tmp}/tmp_corpus.txt" \
+      --batch-size "${batch_size}" --layer-num "${layer_num}" \
+      --out-filename "${output_filename}" --gpu
+
+# remove uncompressed file
+rm -rf "${tmp}"
+
+echo "### Done: Extracted sentence representations for ${input_data}, saved into ${output_filename}"
