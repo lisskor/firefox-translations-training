1c0b8856e609c10a75e46a137f4e9d63f7fec0c3
lisskor <elizaveta.korotkova@gmail.com>
2022-07-17 19:43:21 +0300

    Extract train sentence representation in parts + use minibatch clustering

---
+++ pipeline/clusters/extract_sentence_representations_parts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+##
+# Extract sentence representations for clustering
+#
+
+set -x
+set -euo pipefail
+
+input_data=$1
+hf_teacher_dir=$2
+batch_size=$3
+layer_num=$4
+output_filename=$5
+caching_dir=$6
+
+tmp="${hf_teacher_dir}/tmp"
+mkdir -p "${tmp}"
+
+echo "### Extracting sentence representations for ${input_data}"
+
+# extract representations from given layer
+python 3rd_party/domain_clusters/extract_sentence_representations.py \
+      --hf-model-dir "${hf_teacher_dir}" --txt-dataset-path "${input_data}" \
+      --batch-size "${batch_size}" --layer-num "${layer_num}" \
+      --out-filename "${output_filename}" --caching-dir "${caching_dir}" --gpu
+
+# remove uncompressed file
+rm -rf "${tmp}"
+
+echo "### Done: Extracted sentence representations for ${input_data}, saved into ${output_filename}"
